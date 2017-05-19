c4eb8fa97c0252742ef4143bbf6afe1434b3e1c7
gunhappy <gunhappy123@gmail.com>
2017-05-19 20:12:36 +0700

    camera follow player

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
+	public GameObject player;
+	public Vector3 offset;
 	// Use this for initialization
 	void Start () {
-		
+		offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position = player.transform.position + offset;
 	}
 }
