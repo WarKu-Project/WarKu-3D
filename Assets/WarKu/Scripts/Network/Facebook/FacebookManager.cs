431f5a9f7f266fd6d5e6a974458d5014625bf3fa
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-05-26 23:10:02 +0700

    Add Facebook

---
+++ Assets/WarKu/Scripts/Network/Facebook/FacebookManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+
+public class FacebookManager : MonoBehaviour {
+
+    void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+
+        }else
+        {
+
+        }
+    }
+
+    void InitCallBack()
+    {
+
+    }
+}
