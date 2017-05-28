8e060155c87c540b5c34090f9d8dd7b72b0b7400
gunhappy <gunhappy123@gmail.com>
2017-05-28 12:54:55 +0700

    add progress bar

---
+++ Assets/Script/CiclularProgress.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CiclularProgress : MonoBehaviour {
+	
+	public int timeToComplete = 3;
+	// Use this for initialization
+	void Start () {
+		//Use this to Start progress
+		StartCoroutine(RadialProgress(timeToComplete));
+	}
+
+	public void updateProcess(float value){
+		gameObject.GetComponent<Renderer>().material.SetFloat("_Progress", value);
+	}
+	
+	IEnumerator RadialProgress(float time)
+	{
+//		float rate = 1 / time;
+//		float i = 0;
+//		while (i < 1)
+//		{
+//			i += Time.deltaTime * rate;
+//			gameObject.GetComponent<Renderer>().material.SetFloat("_Progress", i);
+//			yield return 0;
+//		}
+
+//		gameObject.GetComponent<Renderer>().material.SetFloat("_Progress", i);
+		yield return 0;
+	}
+}
--- Assets/Script/Score.cs
+++ Assets/Script/Score.cs
@@ -8,21 +8,25 @@ public class Score : MonoBehaviour {
 	public GameObject explosion;
 	public GameObject player;
 	private GameObject aura; 
-	public bool chceckCurrentLocation(Vector3 currentLocation){
+	public CiclularProgress process;
+	public bool checkCurrentLocation(Vector3 currentLocation){
 		if(currentLocation.x >= 213.5 && currentLocation.x <= 273.5){
 			if (currentLocation.z >= 216.5 && currentLocation.z <= 270.5){
 				return true;
 			}
-		}
-		return false;
+			else return false;
+		} else return false;
+
 	}
 
 	public void setScore(Vector3 currentLocation){
-		if (chceckCurrentLocation (currentLocation)) {
+		if (checkCurrentLocation (currentLocation)) {
 			score += 0.1;
 			aura = Instantiate (explosion, currentLocation, Quaternion.identity);
+//			process.updateProcess (score);
 			Debug.Log (score);
 		} else {
+			Debug.Log ("gun");
 			score = 0;
 		}
 		Destroy (aura,0.5f);
